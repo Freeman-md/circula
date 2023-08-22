8cdde1309143842dc5d064f23131fd7070ac2866
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2023-08-22 06:35:43 +0100

    UPDATE: highlight active contact in contacts sidebar

--- src/components/ContactCard.tsx
+++ src/components/ContactCard.tsx
@@ -1,20 +1,30 @@
-import { Link } from "react-router-dom"
-import { Contact } from "../types"
-import { generateProfilePhoto } from "../utils"
+import { NavLink } from "react-router-dom";
+import { Contact } from "../types";
+import { generateProfilePhoto } from "../utils";
 
 const ContactCard = ({ contact }: { contact: Contact }) => {
-    const profilePhotoURL = generateProfilePhoto(contact.firstName, contact.lastName)
+    const profilePhotoURL = generateProfilePhoto(contact.firstName, contact.lastName);
+
+    const cardBaseClassName = "flex space-x-4 items-center -mx-2 p-3.5";
+    const activeCardClassName = `${cardBaseClassName} bg-secondary/20`;
+    const inactiveCardClassName = `${cardBaseClassName} transition duration-200 bg-transparent hover:bg-secondary/10 cursor-pointer`;
 
     return (
-        <Link to={`/${contact.id}`} className="flex space-x-4 items-center -mx-2 p-3.5 transition duration-200 bg-transparent hover:bg-secondary/10 cursor-pointer">
-            <div className="h-10 w-10 rounded-full bg-gray-200 bg-cover bg-center" style={{
-                backgroundImage: `url(${profilePhotoURL})`,
-            }}></div>
+        <NavLink
+            to={`/${contact.id}`}
+            className={({ isActive }) => isActive ? activeCardClassName : inactiveCardClassName}
+        >
+            <div
+                className="h-10 w-10 rounded-full bg-gray-200 bg-cover bg-center"
+                style={{
+                    backgroundImage: `url(${profilePhotoURL})`,
+                }}
+            ></div>
             <p>
                 {contact.firstName} {contact.lastName}
             </p>
-        </Link>
-    )
-}
+        </NavLink>
+    );
+};
 
-export default ContactCard
+export default ContactCard;
